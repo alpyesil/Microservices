6480407ae69748180dd4f7ebbcce0dc6f3b3fb32
Alperen Saffet Yeşil <alperensyesil@gmail.com>
2023-08-14 14:00:34 -0700

    Initial commit
    Created project folder and Course.Catalog.Service and Shared folder in Dtos class.

---
+++ Services/Catalog/Course.Services.Catalog/Models/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Course.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.String)] 
+        public string UserId { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonRepresentation(BsonType.String)] 
+        public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+
+        [BsonRepresentation(BsonType.String)] 
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+
+
+        public Feature Feature { get; set; }
+        
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
